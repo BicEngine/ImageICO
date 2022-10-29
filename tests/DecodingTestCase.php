495bf716f02b26358421c6cf8f86478658ee2498
SerafimArts <nesk@xakep.ru>
2022-10-29 18:02:56 +0300

    Add simple ICO test

---
+++ tests/DecodingTestCase.php
@@ -0,0 +1,60 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Bic\Image\Ico;
+
+use Bic\Image\Factory;
+use Bic\Image\FactoryInterface;
+use Bic\Image\Format;
+use Bic\Image\Ico\Tests\TestCase;
+use Bic\Image\ImageInterface;
+
+final class DecodingTestCase extends TestCase
+{
+    private readonly FactoryInterface $images;
+
+    public function setUp(): void
+    {
+        $this->images = new Factory([new IcoDecoder()]);
+
+        parent::setUp();
+    }
+
+    public function iconsDataProvider(): array
+    {
+        return [
+            '32bit (1 icon)' => [__DIR__ . '/stubs/32bit.ico', 1],
+            '24bit (1 icon)' => [__DIR__ . '/stubs/24bit.ico', 1],
+        ];
+    }
+
+    /**
+     * @dataProvider iconsDataProvider
+     */
+    public function testImagesCount(string $pathname, int $count): void
+    {
+        $images = $this->images->fromPathname($pathname);
+
+        $this->assertInstanceOf(\Traversable::class, $images);
+        $this->assertCount($count, $images);
+    }
+
+    public function test32Bit(): void
+    {
+        /** @var ImageInterface $image */
+        [$image] = [...$this->images->fromPathname(__DIR__ . '/stubs/32bit.ico')];
+
+        $this->assertSame(Format::B8G8R8A8, $image->getFormat());
+        $this->assertSame(\file_get_contents(__DIR__ . '/stubs/32bit.bin'), $image->getContents());
+    }
+
+    public function test24Bit(): void
+    {
+        /** @var ImageInterface $image */
+        [$image] = [...$this->images->fromPathname(__DIR__ . '/stubs/24bit.ico')];
+
+        $this->assertSame(Format::B8G8R8, $image->getFormat());
+        $this->assertSame(\file_get_contents(__DIR__ . '/stubs/24bit.bin'), $image->getContents());
+    }
+}
